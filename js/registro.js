d0223f82ca2001e977c0283c7b049eb4f0555a98
irizow <irisrossell@gmail.com>
2024-12-25 11:59:14 +0200

    comment code

--- js/registro.js
+++ js/registro.js
@@ -1,12 +1,12 @@
-import { User } from "./clases.js";
+import { User } from "./clases.js"; //Importar la clase usuario
 
-const registerForm = document.getElementById('register-form');
+const registerForm = document.getElementById('register-form'); 
 registerForm.addEventListener('submit', validateForm);
 
 function validateForm(e) {
-    e.preventDefault();
-    console.log('hola')
-    const name = document.getElementById('name').value;
+    e.preventDefault(); //Evitar la submisión del formulario por defecto
+
+    const name = document.getElementById('name').value; //Sacar todos los valores del formulario
     const surname = document.getElementById('surname').value;
     const address = document.getElementById('address').value;
     const community = document.getElementById('community').value;
@@ -14,10 +14,10 @@ function validateForm(e) {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const passwordPattern = /^(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d]{6,20}$/;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; //Regular expression del email
+    const passwordPattern = /^(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d]{6,20}$/; //Regex de la contraseña (min 6 caracteres, max 20, letras y numeros)
 
-    if(username === '') {
+    if(username === '') { //Si el usuario, nombre o email estan vacíos, parar la submisión del formulario
         alert('Tienes que elegir un nombre de usuario');
         return
     }
@@ -29,21 +29,21 @@ function validateForm(e) {
         alert('Tienes que introducir un email');
         return
     }
-    else if(!emailPattern.test(email)) {
+    else if(!emailPattern.test(email)) { //Si el email o contraseña no siguen el patrón, parar submisión.
         alert('Tienes que elegir un email valido!');
         return
     }
-    if(!passwordPattern.test(password)) {
+    if(!passwordPattern.test(password)) { 
         alert('La contraseña debe tener entre 6 y 20 caracteres y contener letras y numeros');
         return
     }
 
-    const user = new User(name, surname, username, password, email, address, community)
-    let existingUsers = JSON.parse(localStorage.getItem('users')) || [];
-    const duplicateUserName = existingUsers.some((user) => user.username === username);
-    const duplicateEmail = existingUsers.some((user) => user.email === email);
+    const user = new User(name, surname, username, password, email, address, community) //Si los datos están bien, crear un nuevo usuario
+    let existingUsers = JSON.parse(localStorage.getItem('users')) || []; //Sacamos usuarios existentes, si los hay, si no, inicializamos una array vacía
+    const duplicateUserName = existingUsers.some((user) => user.username === username); //Chequeamos si existe un usuario con el nombre de usuario introducido.
+    const duplicateEmail = existingUsers.some((user) => user.email === email); //Chequeamos si existe un usuario con el email introducido
 
-    if(duplicateUserName) {
+    if(duplicateUserName) { //Alertamos en caso de usuario o email duplicado
         alert('Este nombre de usuario ya existe, elige otro')
         return;
     }
@@ -53,11 +53,11 @@ function validateForm(e) {
         return
     }
 
-    existingUsers.push(user);
+    existingUsers.push(user); //Si todo esta OK, hacemos push del usuario a la lista de usuarios existentes.
 
-    localStorage.setItem('users', JSON.stringify(existingUsers));
+    localStorage.setItem('users', JSON.stringify(existingUsers)); //Guardamos nuestro array de objetos en localStorage
 
-    alert('Usuario registrado con exito!');
+    alert('Usuario registrado con exito!'); //Alertamos de que todo está OK
     
-    window.location.href = 'index.html'
+    window.location.href = 'index.html' //Redirigimos al usuario para que haga el login
 }
